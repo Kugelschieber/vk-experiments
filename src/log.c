1a54147c6996ac580bbac79a8b5399b99ff34372
Marvin Blum <6115423+Kugelschieber@users.noreply.github.com>
2021-06-09 14:31:49 +0200

    Fixed log levels and time string function.

--- src/log.c
+++ src/log.c
@@ -5,33 +5,27 @@
 
 int logLevel = 0;
 
-// TODO
-/*const char* timeStr() {
+char* timeNow() {
     time_t now = time(NULL);
     struct tm * p = localtime(&now);
-    char buffer[25];
-    strftime(buffer, 25, "%Y-%m-%d %H:%M:%S", p);
-    //buffer[24] = '\0';
-    const char* str = buffer;
-    return str;
-}*/
+    static char buffer[21];
+    strftime(buffer, 20, "%Y-%m-%d %H:%M:%S", p);
+    buffer[20] = '\0';
+    return buffer;
+}
 
 void vkeSetLogLevel(int level) {
     logLevel = level;
 }
 
 void vkeLogDebug(const char* message) {
-    if(logLevel == 0) {
-        time_t now;
-        time(&now);
-        printf("%s [DEBUG] %s\n", ctime(&now), message);
+    if(logLevel < VKE_LOG_ERROR) {
+        printf("%s [DEBUG] %s\n", timeNow(), message);
     }
 }
 
 void vkeLogError(const char* message) {
-    if(logLevel > 0) {
-        time_t now;
-        time(&now);
-        printf("%s [ERROR] %s\n", ctime(&now), message);
+    if(logLevel < VKE_LOG_ERROR+1) {
+        printf("%s [ERROR] %s\n", timeNow(), message);
     }
 }
