b01f592a249afd6d0cc9a3fb48ee057f5077194b
Marvin Blum <6115423+Kugelschieber@users.noreply.github.com>
2021-06-09 14:29:31 +0200

    Fixed define.

--- src/log.h
+++ src/log.h
@@ -1,8 +1,8 @@
 #ifndef VKE_LOG_H
 #define VKE_LOG_H
 
-#define VKE_LOG_DEBUG = 0
-#define VKE_LOG_ERROR = 1
+#define VKE_LOG_DEBUG 0
+#define VKE_LOG_ERROR 1
 
 // Sets the global log level (VKE_LOG_DEBUG by default).
 void vkeSetLogLevel(int level);
